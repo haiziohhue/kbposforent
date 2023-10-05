54649824d75d51977a4f433509e08920cd515c15
Abla Malki <109620464+AblaMalki@users.noreply.github.com>
2023-10-05 09:39:23 +0100

    add the url to constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // export const API_URL = "https://api.strapi-v4.refine.dev";
-export const API_URL = "http://192.168.1.159:1337";
-// export const API_URL = `${import.meta.env.VITE_API_URL}`;
+// export const API_URL = "http://192.168.1.159:1337";
+export const API_URL = "https://backpos.kbdev.co";
 
 export const TOKEN_KEY = "strapi-jwt-token";
